ea52407125f2f25f22d8519ccc6d2e35ef417a0e
raydegroat <stockholmray@gmail.com>
2023-03-08 09:38:41 +0100

    Adds list of datetime.date One date for each day of the tax year

--- maxval.py
+++ maxval.py
@@ -2,16 +2,29 @@
 # Maxval accounts value evaluator - Raymond de Groat, 2023 https://github.com/raydegroat
 
 import pandas as pd
+import datetime
 from pathlib import Path
 import os
 
+# Directory where bank csv files are stored
 dir_name = 'account_history'
+days = []
 
-year2022 = pd.date_range(start='1/1/2022', end='12/31/2022')
+# Using datetime to make a list of days in the year
+Jan1_2022 = datetime.date(2022, 1, 1 )
+Dec31_2022 = datetime.date(2022, 12, 31)
+aday = datetime.timedelta(days=1)
+thedate = Jan1_2022
+
+while (thedate <= Dec31_2022):
+    days.append(thedate)
+    thedate += aday
+
 
 # Use Path to get files ending in .csv
 dir_list = Path(dir_name).glob('*.csv')
 for file in dir_list:
+
     print(file.name)
     f = open(file, 'r')
     lines = f.readlines()
